d8126014498388654fd9cb40618519b155075663
Said Yeter <kordiseps@gmail.com>
2025-11-24 00:20:59 +0300

    fix imports

--- __root.tsx
+++ __root.tsx
@@ -1,9 +1,14 @@
+import css1 from '@/assets/govuk-frontend-5.11.0.min.css?url'
+import css2 from '@/assets/jquery-ui-custom.css?url'
+import logo from '@/assets/logo.svg'
+import css3 from '@/assets/screen.css?url'
+import Header from '@/components/Header'
+import Footer from '@/components/footer'
+import appCss from '@/styles.css?url'
+
 import { TanStackDevtools } from '@tanstack/react-devtools'
-import { Scripts, createRootRoute } from '@tanstack/react-router'
+import { HeadContent, Scripts, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
-import Header from '../components/Header'
-import Footer from '../components/footer'
-import appCss from '../styles.css?url'
 
 export const Route = createRootRoute({
   head: () => ({
@@ -16,14 +21,15 @@ export const Route = createRootRoute({
         content: 'width=device-width, initial-scale=1',
       },
       {
-        title: 'TanStack Start Starter',
+        title: 'Access your booking - Change booking',
       },
     ],
     links: [
-      {
-        rel: 'stylesheet',
-        href: appCss,
-      },
+      { rel: 'stylesheet', href: appCss, },
+      { rel: 'stylesheet', href: css1, },
+      { rel: 'stylesheet', href: css2, },
+      { rel: 'stylesheet', href: css3 },
+      { rel: 'icon', sizes: '48x48', href: logo },
     ],
   }),
 
@@ -34,22 +40,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
-        <meta httpEquiv="Content-Type" content="text/html; charset=UTF-8" />
-        <meta content="True" name="HandheldFriendly" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
-        <meta name="viewport" content="width=device-width,minimum-scale=1,maximum-scale=1" />
-        <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="robots" content="noindex,nofollow" />
-        <meta name="dcterms.isVersionOf" content="IBS-6.3.0.30" />
-        <link rel="icon" sizes="48x48" href="./src/assets/logo.svg" />
-        <link rel="stylesheet" href="./src/assets/govuk-frontend-5.11.0.min.css" />
-        <link rel="stylesheet" href="./src/assets/jquery-ui-custom.css" />
-        <link rel="stylesheet" href="./src/assets/screen.css" />
-        <title>
-          Access your booking
-          - Change booking
-        </title>
-        <meta name="theme-color" content="#1d70b8" />
+        <HeadContent />
       </head>
       <body>
         <Header />
