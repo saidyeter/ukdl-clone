5ae8b672de17bde11bc851b7c72ef4202e2fa764
Said Yeter <kordiseps@gmail.com>
2025-11-25 10:17:22 +0300

    fix css imports

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,15 +2,9 @@ import { TanStackDevtools } from '@tanstack/react-devtools'
 import { HeadContent, Scripts, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 
-
 import logo from '@/assets/logo.svg'
 import Footer from '../components/footer'
 import Header from '../components/header'
-
-import css1 from "../assets/govuk-frontend-5.11.0.min.css?url"
-import css2 from "../assets/jquery-ui-custom.css?url"
-import css3 from "../assets/login.css?url"
-import css4 from "../assets/standby.css?url"
 import appCss from '../styles.css?url'
 
 export const Route = createRootRoute({
@@ -29,10 +23,6 @@ export const Route = createRootRoute({
     ],
     links: [
       { rel: 'stylesheet', href: appCss, },
-      { rel: 'stylesheet', href: css1, },
-      { rel: 'stylesheet', href: css2, },
-      { rel: 'stylesheet', href: css3, },
-      { rel: 'stylesheet', href: css4, },
       { rel: 'icon', 'size': '48x48', href: logo },
     ],
   }),
