e3f9d5d9a44de38619e85e099e0d3c8510750408
Said Yeter <kordiseps@gmail.com>
2025-11-25 23:21:53 +0300

    add next js setup

---
+++ app/manage/page.tsx
@@ -0,0 +1,198 @@
+'use client'
+import testCenters from '@/app/test-centres.json';
+import testData from '@/app/test-data.json';
+import { useLicenceStore } from "@/lib/store";
+import { useRouter } from "next/navigation";
+
+export default function RouteComponent() {
+  const { push } = useRouter();
+
+  const {
+    licenceNumber, //theoryTestPassNumber, referenceNumber,
+    // setLicenceNumber, setTheoryTestPassNumber, setReferenceNumber,
+  } = useLicenceStore();
+  // console.log(licenceNumber, theoryTestPassNumber, referenceNumber);
+  const booking = testData.find(a => a.driverLicenceNumber === licenceNumber);
+  const testCentre = testCenters.find(a => a.zipcode === booking?.testCentre);
+  if (!licenceNumber || !booking || !testCentre) {
+    push('/not-found');
+    return
+  }
+
+  async function handleSignout(e: React.MouseEvent<HTMLAnchorElement>) {
+    e.preventDefault();
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    push('/');
+    return
+  }
+
+  const cancelled = booking.status.toLocaleLowerCase() === "cancelled";
+  const closeDate = booking.isDateClose;
+
+  return (
+    <>
+
+      <div id="page" data-journey="pp-change-practical-driving-test-public:change-booking">
+
+
+        <section aria-label="Header proposition">
+          <div className="header-proposition-container">
+            <span className="header-proposition-name">
+              Change your driving test
+            </span>
+          </div>
+        </section>
+
+
+        <main id="main" role="main">
+          <header className="booking-change-incomplete">
+            <div id="header-title">
+              <h1>View booking</h1>
+              <dl>
+                <dt>Driving licence number:</dt>
+                <dd>{licenceNumber}</dd>
+              </dl>
+            </div>
+            <div id="header-buttons">
+              <div id="header-button-container">
+                <a onClick={handleSignout} className="button">Sign out</a>
+              </div>
+            </div>
+          </header>
+
+          <section id="confirm-booking-details" className="formatting">
+
+            {
+              cancelled ? (
+                <header>
+                  <div className="onscreen-help">
+                    <p>Your booking has been cancelled. You’ll need to either re-book your test or call the <abbr title="Driver and Vehicle Standards Agency">DVSA</abbr> on 0300 200 1122. </p>
+                  </div>
+                </header>
+
+              ) : (closeDate ? (
+
+                <header>
+                  <div className="onscreen-help">
+                    <p>You’ll need to call the <abbr title="Driver and Vehicle Standards Agency">DVSA</abbr> on 0300 200 1122 if you want to make any changes to this application.</p>
+                  </div>
+                </header>
+              ) : null)}
+            {!cancelled ? (
+              <>
+                <section>
+                  <header>
+                    <h2>Date and time of test</h2>
+                    {!closeDate ? (
+                      <a href="#" className="button">Change</a>
+                    ) : null}
+                    {!closeDate ? (
+                      <a href="#" className="button">Find tests within 3 days</a>
+                    ) : null}
+                  </header>
+
+                  <div className="contents">
+                    <dl>
+                      <dd>{new Date(booking.date).toLocaleString('en-GB', { day: 'numeric', month: 'long', year: 'numeric' })} {new Date(booking.date).toLocaleString('en-GB', { hour: 'numeric', minute: 'numeric' })}pm</dd>
+
+                      <dd>This test slot is non-refundable</dd>
+
+                    </dl>
+                  </div>
+                </section>
+
+                <section>
+                  <header>
+                    <h2>Test centre</h2>
+
+                    {!closeDate ? (
+                      <a href="/change-test-centre" className="button">Change</a>
+                    ) : null}
+                  </header>
+
+                  <div className="contents">
+                    <dl>
+                      <dd>{testCentre?.name}</dd>
+                    </dl>
+                    <span className="clear"></span>
+                    <span className="google-map-link"><a href={testCentre?.google_maps_link} title="Opens in a new window" rel="external"
+                      target="_blank">Google map</a></span>
+
+
+                    <div id="centre-facilities-info" className="formatting clear visuallyhidden reveal"
+                      style={{ overflow: 'hidden', display: 'block' }}
+                    >
+                      <h3> Facilities </h3>
+
+                      <dl>
+                        <dt>Toilet:</dt>
+                        <dd>Disabled toilets are available</dd>
+
+                        <dt>Disabled access:</dt>
+                        <dd>Disabled parking and wheelchair access</dd>
+
+                        <dt>Parking:</dt>
+                        <dd>A car park is available at the test centre</dd>
+
+                      </dl>
+
+                      <p className="clear"></p>
+                    </div>
+                  </div>
+                </section>
+              </>
+            ) : null}
+            <section>
+              <header>
+                <h2>Type of test</h2>
+
+                {(!closeDate && !cancelled) ? (
+                  <a href="#" className="button">Change</a>
+                ) : null}
+              </header>
+
+              <div className="contents">
+                <dl>
+                  <dd>Car</dd>
+
+                  <dt>Fee</dt>
+                  <dd>£62.00</dd>
+
+                  <dt>Test status</dt>
+                  <dd>Booked</dd>
+                  <dt>Extended test</dt>
+                  <dd>
+                    No
+                  </dd>
+                </dl>
+              </div>
+            </section>
+
+
+            <section>
+              <header>
+                <h2>Instructor’s reference number</h2>
+
+                {(!closeDate && !cancelled) ? (
+                  <a href="#" className="button">Change</a>
+                ) : null}
+              </header>
+
+              <div className="contents">
+                <dl>
+                  <dt className="visuallyhidden"></dt>
+                  <dd>
+                    Not entered
+                  </dd>
+                </dl>
+              </div>
+            </section>
+
+          </section>
+
+        </main>
+
+      </div>
+    </>
+  )
+}
---
+++ change-test-centre/page.tsx
@@ -0,0 +1,107 @@
+"use client"
+import testCenters from '@/app/test-centres.json';
+import testdata from '@/app/test-data.json';
+import { useLicenceStore } from '@/lib/store';
+import { useRouter } from 'next/navigation';
+
+
+export default function RouteComponent() {
+  const { push } = useRouter();
+  const {
+    licenceNumber, theoryTestPassNumber, referenceNumber,
+    // setLicenceNumber, setTheoryTestPassNumber, setReferenceNumber,
+  } = useLicenceStore();
+  // console.log(licenceNumber, theoryTestPassNumber, referenceNumber);
+  const booking = testdata.find(a => a.driverLicenceNumber === licenceNumber);
+  const testCentre = testCenters.find(a => a.zipcode === booking?.testCentre);
+  if (!licenceNumber || !booking || !testCentre) {
+    console.log(licenceNumber, theoryTestPassNumber, referenceNumber);
+    // return window.location.href = '/not-found';
+    return null
+  }
+
+  // async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+  //   e.preventDefault();
+  //   await new Promise(resolve => setTimeout(resolve, 1000));
+  // }
+
+  const cancelled = booking.status.toLocaleLowerCase() === "cancelled";
+  const closeDate = booking.isDateClose;
+
+  if (cancelled || closeDate) {
+    push('/manage');
+  }
+
+  return (
+    <>
+
+      <div id="page" data-journey="pp-change-practical-driving-test-public:change-booking">
+        <section aria-label="Header proposition">
+          <div className="header-proposition-container">
+            <span className="header-proposition-name">
+              Change your driving test
+            </span>
+          </div>
+        </section>
+        <main id="main" role="main">
+
+
+          <div id="progress-bar-wrapper">
+            <div id="progress-bar" role="progressbar">
+              <h1 className="visuallyhidden">Your progression through this transaction</h1>
+
+              <strong className="mobile">Step <span id="progress-button" className="button" role="button">1 of 4</span></strong>
+
+              <ol id="process-steps" className="mobilehidden">
+                <li id="status" aria-controls="progress-bar">
+                  Test centre</li>
+                <li>
+                  Test time</li>
+                <li className="complete">
+                  Card details</li>
+                <li className="end-point">
+                  Booking summary</li>
+              </ol>
+            </div>
+          </div>
+          <div className="page-header">
+            <h1>Test centre <span className="visuallyhidden">search and results</span></h1>
+
+          </div>
+          <section>
+            <form method="post" action="/manage?execution=e2s10&amp;_eventId=search" className="" autoComplete="on">
+              <fieldset className="test-centres-find">
+                <legend>
+                  Search by your home postcode or by test centre name
+                </legend>
+
+                <div className="form-block search formatting example">
+
+                  <label htmlFor="test-centres-input">
+                    <span className="label hidden">
+                      Search by your home postcode or by test centre name
+                    </span>
+
+                    <input type="text" name="testCentreName" id="test-centres-input" className="text" value="Barnsley"
+                      maxLength={50} />
+                    <small className="example">
+                      Example: NG1 6LP <i>or</i> NG1 <i>or</i> Nottingham or Nott
+                    </small>
+                  </label>
+
+
+                  <input type="submit" name="testCentreSubmit" id="test-centres-submit" value="Find test centres"
+                    className="button cta" />
+
+
+                </div>
+              </fieldset>
+            </form>
+
+          </section>
+        </main>
+
+      </div>
+    </>
+  )
+}
